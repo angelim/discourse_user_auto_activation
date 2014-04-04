7e820c653cb3a95d88425ed417e3d8b4d33b2bbb
Alexandre Angelim <angelim@angelim.com.br>
2014-04-04 00:29:49 -0300

    skip email token creation

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,13 @@ after_initialize do
   # Rails Engine for accepting votes.
   module AutoActivationPlugin
 	  require_dependency 'users_controller'
+	  require_dependency 'user'
+
+	  User.class_eval do
+		  def create_email_token
+		    email_tokens.create(email: email) unless SiteSetting.auto_activation_enabled
+		  end
+	  end
 	  UsersController.class_eval do
 			private
 		    def user_params
